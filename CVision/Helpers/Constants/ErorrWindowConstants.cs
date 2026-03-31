4dee6ec3a7e4dbb45ca885ff61ccce7b287595dc
Olekselot <loplesyk@gmail.com>
2026-03-31 23:06:03 +0300

    Update error window constants for consistency and clarity

--- CVision/Helpers/Constants/ErorrWindowConstants.cs
+++ CVision/Helpers/Constants/ErorrWindowConstants.cs
@@ -2,7 +2,9 @@ namespace CVision.Helpers.Constants;
 
 public static class ErorrWindowConstants
 {
-    public static readonly string WindowTitle = "Помилки";
+    public static readonly string Title = "Сталася помилка";
 
-    public static readonly string WindowAriaLabel = "Вікно помилок";
+    public static readonly string CloseButtonText = "Закрити";
+
+    public static readonly string AriaLabel = "Вікно помилки";
 }
